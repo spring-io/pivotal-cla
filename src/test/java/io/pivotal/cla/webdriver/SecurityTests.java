3de3bf6308b4a6869a8a9d3381ccf0729cb5e353
Rob Winch <rwinch@gopivotal.com>
2016-02-05 08:42:48 -0600

    Remove unused imports

--- src/test/java/io/pivotal/cla/webdriver/SecurityTests.java
+++ src/test/java/io/pivotal/cla/webdriver/SecurityTests.java
@@ -21,7 +21,6 @@ import static org.mockito.Matchers.anySet;
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
