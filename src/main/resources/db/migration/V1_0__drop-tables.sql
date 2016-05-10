665245018348428a8eb669dfadf2fe933e504d4c
Rob Winch <rwinch@gopivotal.com>
2016-05-10 13:51:16 -0500

    Polish Migration
    * Add drop tables
    * Remove CLA inserts (done as part of migration instead)

---
+++ src/main/resources/db/migration/V1_0__drop-tables.sql
@@ -0,0 +1,13 @@
+-- DROP TABLE IF EXISTS schema_version CASCADE;
+
+DROP TABLE IF EXISTS access_token CASCADE;
+
+DROP TABLE IF EXISTS corporate_signature CASCADE;
+
+DROP TABLE IF EXISTS individual_signature;
+
+DROP TABLE IF EXISTS user_email CASCADE;
+
+DROP TABLE IF EXISTS user CASCADE;
+
+DROP TABLE IF EXISTS contributor_license_agreement CASCADE;
