cd3ce37bbd0291d26049a0de1cefa825665f37a1
Rob Winch <rwinch@gopivotal.com>
2016-05-31 14:43:58 -0500

    Remove unused import

--- src/test/java/io/pivotal/cla/webdriver/pages/admin/AdminLinkClaPage.java
+++ src/test/java/io/pivotal/cla/webdriver/pages/admin/AdminLinkClaPage.java
@@ -17,7 +17,6 @@ package io.pivotal.cla.webdriver.pages.admin;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
