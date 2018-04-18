a54c4a8e1c858549a19158c14312041c9361ebb6
Rob Winch <rwinch@users.noreply.github.com>
2018-04-18 15:48:49 -0500

    Update PATH in install-cf.sh

--- ci/scripts/download-cf.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-curl -L "https://cli.run.pivotal.io/stable?release=linux64-binary&source=github" | tar -zx
---
+++ ci/scripts/install-cf.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+curl -L "https://cli.run.pivotal.io/stable?release=linux64-binary&source=github" | tar -zx
+export PATH=$PATH:$PWD
